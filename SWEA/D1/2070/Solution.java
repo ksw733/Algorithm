565d0afb05b21078bcf9be1ca2bfa220f1b4ca34
Kim, Seokwan <sukwan1106@gmail.com>
2024-07-07 01:48:55 +0900

    2070

---
+++ SWEA/D1/2070/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a<b){
+                System.out.println("#"+test_case+" <");
+            } else if (a==b){
+                System.out.println("#"+test_case+" =");
+
+            } else {
+                System.out.println("#"+test_case+" >");
+            }
+
+        }
+    }
+}
