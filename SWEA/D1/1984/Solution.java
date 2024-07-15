feeade38b655aac894b5a3ff94ddfc9e7018b88f
Kim, Seokwan <sukwan1106@gmail.com>
2024-07-15 11:57:09 +0900

    1984

--- SWEA/D1/1984/Solution.java
+++ SWEA/D1/1984/Solution.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 import java.io.FileInputStream;
 
 class Solution
@@ -11,17 +12,17 @@ class Solution
 
         for(int test_case = 1; test_case <= T; test_case++)
         {
-            int n = sc.nextInt();
-            int result = 0;
-            for(int i=1; i<=n; i++){
-                if(i%2==0){
-                    result -= i;
-                } else {
-                    result += i;
-                }
+            int[] arr = new int[10];
+            for(int i=0; i<arr.length; i++){
+                arr[i] = sc.nextInt();
             }
-            System.out.println("#"+test_case+" "+result);
+            int result = 0;
+            Arrays.sort(arr);
+            for(int i=1; i<arr.length-1; i++){
+                result += arr[i];
+            }
+            System.out.println("#"+test_case+" "+Math.round(result/8.0));
+
         }
     }
 }
-
